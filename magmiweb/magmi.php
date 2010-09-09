fed95b07f57a38e942abb1c24f3ed8676315f38f
Sebastien Bracquemont <dweeves@gmail.com>
2010-09-09 12:31:44 +0000

    initial commit
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk/magmi@2 8a1fb3a6-a994-4f0a-9076-b49fc636424d

---
+++ magmiweb/magmi.php
@@ -0,0 +1,57 @@
+<?php
+	ini_set('include_path',ini_get('include_path').":..");
+	require_once("magento_mass_importer.class.php");
+	require_once("fshelper.php");
+	
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<title>Magento Mass Importer by Dweeves - version <?php echo MagentoMassImporter::$version ?></title>
+<link rel="stylesheet" href="css/960.css"></link>
+<link rel="stylesheet" href="css/reset.css"></link>
+<link rel="stylesheet" href="css/magmi.css"></link>
+<script type="text/javascript" src="../../js/prototype/prototype.js"></script>
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<META HTTP-EQUIV="Expires" CONTENT="-1">
+</head>
+<body>
+<div class="container_12">
+	<div class="grid_12 title  omega">
+		<span>
+  		Magento Mass Importer by Dweeves
+ 		</span>
+ 		<span class="version">
+ 		version <?php echo MagentoMassImporter::$version ?>
+ 		</span>
+	</div>
+	<div class="clear"></div>
+</div>
+<?php
+	if(FSHelper::isDirWritable(".."))
+	{
+	
+		if(isset($_REQUEST["run"]) && file_exists("../magento_mass_importer.ini"))
+		{
+			unset($mmi);
+			require_once("magmi_import.php");
+		}
+		else
+		{
+			require_once("magmi_config.php");
+		}
+	}
+	else
+	{
+		?>
+	<div class="container_12 config_error">
+		Directory permissions not compatible with Mass Importer operations
+		<br/>
+		PHP/Web Server must have write permissions to magmi directory
+	</div>
+		<?php 
+	}
+?>
+</body>
+</html>
---
+++ magmiweb/magmi_config.php
@@ -0,0 +1,73 @@
+<?php
+$t=0;
+?>
+<div class="container_12">
+
+	<div class="grid_12 subtitle">Configuration</div>
+</div>
+<div class="clear"></div>
+
+<?php 
+
+$cnf="../magento_mass_importer.ini";
+if(!file_exists("$cnf"))
+{
+	$cnf="../magento_mass_importer.ini.default";
+}
+$props=new Properties();
+$props->load($cnf);
+?>
+<form method="post" action="magmi_saveconfig.php">
+<div class="container_12">
+	<div class="grid_4 col">
+	<h3>Database</h3>
+	<ul class="formline">
+		<li class="label">Name:</li>
+		<li class="value"><input type="text" name="DATABASE:dbname" value="<?php echo $props->get("DATABASE","dbname")?>" ></input></li>
+	</ul>
+	<ul class="formline">
+		<li class="label">Host:</li>
+		<li class="value"><input type="text" name="DATABASE:host" value="<?php echo $props->get("DATABASE","host")?>" ></input></li>
+	</ul>
+	<ul class="formline">
+		<li class="label">Username:</li>
+		<li class="value"><input type="text" name="DATABASE:user" value="<?php echo $props->get("DATABASE","user")?>" ></input></li>
+	</ul>
+	<ul class="formline">
+		<li class="label">Password:</li>
+		<li class="value"><input type="text" name="DATABASE:password" value="<?php echo $props->get("DATABASE","password")?>" ></input></li>
+	</ul>
+	<ul class="formline">
+		<li class="label">Table prefix:</li>
+		<li class="value"><input type="text" name="DATABASE:table_prefix" value="<?php echo $props->get("DATABASE","table_prefix")?>" ></input></li>
+	</ul>
+	</div>
+	<div class="grid_4 col">
+	<h3>Magento</h3>
+	<ul class="formline">
+		<li class="label">Base dir:</li>
+		<li class="value"><input type="text" name="MAGENTO:basedir" value="<?php echo $props->get("MAGENTO","basedir")?>" ></input></li>
+	</ul>
+	<ul class="formline">
+		<li class="label">Enabled Status:</li>
+		<li class="value"><input type="text" name="MAGENTO:enabled_status_label" value="<?php echo $props->get("MAGENTO","enabled_status_label","Enabled")?>"></input></li>
+	</ul>
+	</div>
+	<div class="grid_4 col omega">
+	<h3>Global</h3>
+	<ul class="formline">
+		<li class="label">Reporting step:</li>
+		<li class="value"><input type="text" name="GLOBAL:step" value="<?php echo $props->get("GLOBAL","step")?>"></input></li>
+	</ul>
+	<ul class="formline">
+		<li class="label">CSV separator:</li>
+		<li class="value"><input type="text" name="GLOBAL:csv_separator" value="<?php echo $props->get("GLOBAL","csv_separator")?>"></input></li>
+	</ul>
+	</div>
+	<div class="clear"></div>
+	
+	<div class="grid_4 omega push_11">
+		<input type="submit"></input>
+	</div>
+	</div>
+</form>
