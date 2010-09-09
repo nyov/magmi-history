af04d684b027389a456a5105a7d384071abd0235
dweeves <dweeves@gmail.com>
2010-09-09 22:53:00 +0000

    plugin architecture refactoring, functional ok gui to complete
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@3 8a1fb3a6-a994-4f0a-9076-b49fc636424d

---
+++ magmi/plugins/datasources/magmi_csvdatasource.php
@@ -0,0 +1,92 @@
+<?php
+class CSVException extends Exception
+{
+	
+}
+
+class Magmi_CSVDataSource extends Magmi_Datasource
+{
+	protected $_filename;
+	protected $_fh;
+	protected $_cols;
+	protected $_csep;
+	
+	public function initialize($params=null)
+	{
+		$this->_filename=$params["filename"];	
+		if(!isset($this->_filename))
+		{
+			throw new CSVException("No csv file set");
+		}
+		if(!file_exists($this->_filename))
+		{
+			throw new CSVException("{$this->_filename} not found");
+		}
+		$this->_csep=$this->getParam("csv_separator",",");
+	}
+	
+	public function getPluginInfo()
+	{
+		return array("name"=>"CSV Datasource",
+					 "author"=>"Dweeves",
+					 "version"=>"1.0");
+	}
+	
+	public function getRecordsCount()
+	{
+		//open csv file
+		$f=fopen($this->_filename,"rb");
+		//get records count
+		$count=-1;
+		while(fgetcsv($f,4096,$this->_csep))
+		{
+			$count++;
+		}
+		fclose($f);
+		return $count;
+	}
+	
+	public function getAttributeList()
+	{
+		
+	}
+	
+	public function beforeImport()
+	{
+		
+	}
+	
+	public function afterImport()
+	{
+		
+	}
+	
+	public function startImport()
+	{
+		//open csv file
+		$this->_fh=fopen($this->_filename,"rb");
+	}
+	
+	public function getColumnNames()
+	{
+		$this->_cols=fgetcsv($this->_fh,4096,$this->_csep,'"');
+		return $this->_cols;
+	}
+	
+	public function endImport()
+	{
+		fclose($this->_fh);	
+	}
+	
+	public function getNextRecord()
+	{
+		$row=fgetcsv($this->_fh,4096,$this->_csep,'"');
+		if($row===false)
+		{
+			return false;
+		}
+		//create product attributes values array indexed by attribute code
+		$record=array_combine($this->_cols,$row);
+		return $record;
+	}
+}
---
+++ magmi/plugins/inc/magmi_datasource.class.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class Magmi_Datasource
+ * @author dweeves
+ *
+ * This class enables to perform input format support for ingested data
+ */ 
+ require_once("magmi_plugin.php");
+abstract class Magmi_DataSource extends Magmi_Plugin
+{
+	
+	public function getColumnNames()
+	{
+		
+	}
+	public function getRecordsCount()
+	{
+		
+	}
+	
+	public function beforeImport()
+	{
+		
+	}
+	
+	public function afterImport()
+	{
+		
+	}
+	
+	public function getNextRecord()
+	{
+		
+	}
+}
