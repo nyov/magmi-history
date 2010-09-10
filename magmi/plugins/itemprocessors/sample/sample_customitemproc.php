eb507cb60deab0199e66163dae6b629aaccf6b69
pablopla <pablopla@users.sourceforge.net>
2010-09-10 22:30:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@30 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/plugins/itemprocessors/sample/sample_customitemproc.php
+++ magmi/plugins/itemprocessors/sample/sample_customitemproc.php
@@ -8,7 +8,14 @@
 class SampleCustomItemProcessor extends Magmi_ItemProcessor
 {
 
-
+    public function getPluginInfo()
+    {
+        return array(
+            "name" => "Sample Custom",
+            "author" => "Your Name",
+            "version" => "0.0.1"
+        );
+    }
 	
 	/**
 	 * you can add/remove columns for the item passed since it is passed by reference
@@ -75,10 +82,11 @@ class SampleCustomItemProcessor extends Magmi_ItemProcessor
 	
 	public function initialize($params)
 	{
-		
+		return true;
 	}
 	
 	public function processColumnList(&$cols,$params=null)
 	{
+        return true;
 	}
 }
