e35a22f87eb6aa1b4e45181fbea92e7e3fd3fe93
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2010-09-10 16:21:59 +0000

    implemented pluggable attribute handlers
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk/magmi@10 69904d63-209b-40b2-b39f-34af9802e1b1

---
+++ plugins/inc/magmi_attributehandler.php
@@ -0,0 +1,25 @@
+<?php
+class Magmi_AttributeHandler
+{
+	/*	
+	public function handleDecimalAttribute($pid,$storeid,$ivalue,$attrdesc)
+	{
+		
+	}
+	
+	public function handleDatetimeAttribute($pid,$storeid,$ivalue,$attrdesc)
+	{
+		
+	}
+	
+	public function handleIntAttribute($pid,$storeid,$ivalue,$attrdesc)
+	{
+		
+	}
+	
+	public function handleVarcharAttribute($pid,$storeid,$ivalue,$attrdesc)
+	{
+		
+	}
+	*/
+}
