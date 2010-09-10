3a99dbbcb496ce3950916c12e328aa8da6d86ed0
pablopla <pablopla@69904d63-209b-40b2-b39f-34af9802e1b1>
2010-09-10 21:17:10 +0000

    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@22 69904d63-209b-40b2-b39f-34af9802e1b1

--- magmi/plugins/itemprocessors/pablo/pablo_categorynames.php
+++ magmi/plugins/itemprocessors/pablo/pablo_categorynames.php
@@ -9,30 +9,32 @@ class CategoryNamesItemProcessor extends Magmi_ItemProcessor
 {
     protected $_categories;
 
-	public function beforeImport()
-	{
-        $t1 = $this->tablename("catalog_category_entity_varchar");
-        $t2 = $this->tablename("eav_attribute");
-
-        $sql = "Select entity_id, value FROM $t1
-                JOIN $t2 ON $t2.attribute_id=$t1.attribute_id
-                AND $t2.entity_type_id=$t1.entity_type_id
-                AND $t2.attribute_code='name'";
-        $result=$this->selectAll($sql);
-        foreach($result as $r)
-        {
-            $this->_categories[$r['value']] = $r['entity_id'];
-        }
-		return true;
-	}
-
-	public function afterImport()
-	{
-		return true;
-	}
+    public function getPluginInfo()
+    {
+        return array(
+            "name" => "Category Names",
+            "author" => "Pablo",
+            "version" => "0.0.1"
+        );
+    }
 
 	public function processItemBeforeId(&$item,$params=null)
 	{
+        // categories
+        $item['category_ids'] = $this->getCategories($mmi, $item);
+        unset($item['category_names']);
+
+        if(isset($item['category_names'])) {
+            $categories = $item['category_names'];
+            $categories = explode(',', $categories);
+            $categoryIds = array();
+            foreach($categories as $category) {
+                $categoryIds[] = $this->_categories[trim($category)];
+            }
+            $item['category_ids'] = implode(',', $categoryIds);
+            unset($item['category_names']);
+        }
+
 		return true;
 	}
 
@@ -49,11 +51,25 @@ class CategoryNamesItemProcessor extends Magmi_ItemProcessor
 
 	public function initialize($params)
 	{
+        $t1 = $this->tablename("catalog_category_entity_varchar");
+        $t2 = $this->tablename("eav_attribute");
 
+        $sql = "Select entity_id, value FROM $t1
+                JOIN $t2 ON $t2.attribute_id=$t1.attribute_id
+                AND $t2.entity_type_id=$t1.entity_type_id
+                AND $t2.attribute_code='name'";
+        $result=$this->selectAll($sql);
+        foreach($result as $r)
+        {
+            $this->_categories[$r['value']] = $r['entity_id'];
+        }
+		return true;
 	}
 
 	public function processColumnList(&$cols,$params=null)
 	{
-
+        if(!in_array('category_ids', $cols)) {
+            $cols[] = 'category_ids';
+        }
 	}
 }
