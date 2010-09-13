18454c03774559478ee17e0c7841d93fe68d597e
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2010-09-13 00:49:58 +0000

    enhanced progress display
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk/magmi@44 69904d63-209b-40b2-b39f-34af9802e1b1

--- web/magmi_progress.php
+++ web/magmi_progress.php
@@ -7,24 +7,24 @@ try
 	$out="";
 	$skipped=0;
 	//avoid warning
-		$out="initializing...";
-		if(Magmi_StateManager::getState()=="canceled")
-		{
+	$out="initializing...";
+	if(Magmi_StateManager::getState()=="canceled")
+	{
 		$out="";
-			$out.="<div class='log_warning'>Canceled by user</div>";
-			$out.="<div class='log_warning'>";
-			$out.="<span><a href='magmi.php'>Back to Configuration Page</a></span>";
-			$out.="<span><a href='magmi.php?run=1'>Back to Import</a></span>";
-			$out.="</div>";
-			$out.="<script type=\"text/javascript\">endImport();</script>";
-		}
-		else
+		$out.="<div class='log_warning'>Canceled by user</div>";
+		$out.="<div class='log_warning'>";
+		$out.="<span><a href='magmi.php'>Back to Configuration Page</a></span>";
+		$out.="<span><a href='magmi.php?run=1'>Back to Import</a></span>";
+		$out.="</div>";
+		$out.="<script type=\"text/javascript\">endImport();</script>";
+	}
+	else
+	{
+		if(file_exists("../state/tmp_out.txt"))
 		{
-			if(file_exists("../state/tmp_out.txt"))
-			{
-				$data=file_get_contents("../state/tmp_out.txt");
-			}
+			$data=file_get_contents("../state/tmp_out.txt");
 		}
+	}
 	if($data)
 	{
 		$out.="<div class='log_start'>Import Started</div>";
@@ -36,65 +36,54 @@ try
 		$ended=false;
 		$count=0;
 		$nlines=-1;
+		$sout="";
 		foreach($lines as $line)
 		{
 			if($line!="")
 			{
 				list($type,$info)=explode(":",$line,2);
-				if($type=="lookup")
-				{
-					list($nlines,$time)=explode(":",$info);
-				}
-				if($type=="step")
-				{
-					list($label,$step)=explode(":",$info);
+				switch($type){
+					case "title":
+						break;
+					case "pluginhello":
+						list($ptype,$data)=explode(":",$info,2);
+						list($name,$ver,$auth)=explode("-",$data);
+						$out.="<div class='pluginhello'>$name v$ver by $auth</div>";
+						break;
+					case "lookup":
+						list($nlines,$time)=explode(":",$info);
+						break;
+					case "step":
+						list($label,$step)=explode(":",$info);
+						break;
+					case "itime":
+						$parts=explode("-",$info);
+						list($count,$elapsed,$lastinc)=array(trim($parts[0]),trim($parts[1]),trim($parts[2]));
+						$speed = ceil(($count*60)/$elapsed);
+						$elapsed=round($elapsed,4);
+						$lastinc=round($lastinc,4);
+						break;
+					case "error":
+						$errors[]=$info;
+						break;
+					case "warning":
+						$warnings[]=$info;
+						break;
+					case "end":
+						$ended=true;
+						break;
+					case "skip":
+						$skipped+=1;
+						break;
+					default:
+						$sout.="<div class='log_standard'>$info</div>";
+						break;
 				}
 
-				if($type=="itime")
-				{
-					$parts=explode("-",$info);
-					list($count,$elapsed,$lastinc)=array(trim($parts[0]),trim($parts[1]),trim($parts[2]));
-					$speed = ceil(($count*60)/$elapsed);
-					$elapsed=round($elapsed,4);
-					$lastinc=round($lastinc,4);
-				}
-				if($type=="error")
-				{
-					$errors[]=$info;
-				}
-				if($type=="warning")
-				{
-					$warnings[]=$info;
-				}
-				if($type=="indexing")
-				{
-					if(substr($info,0,7)=="Reindex")
-					{
-						$curindex=$info;
-						$indexes[$info]="";
-					}
-					else
-					{
-						$indexes[$curindex]=$info;
-					}
-				}
-				if($type=="end")
-				{
-					$ended=true;
-				}
-				if($type=="skip")
-				{
-					$skipped+=1;
-				}
 			}
-
 		}
 		if($nlines>0)
 		{
-			if($nlines<$step)
-			{
-			
-			}
 			$percent=round(((float)$count*100)/$nlines,2);
 			$out.="<script type=\"text/javascript\">setProgress($percent);</script>";
 			if($count)
@@ -107,36 +96,33 @@ try
 				}
 				$out.="imported $count items ($percent %) in $elapsed secs (last $step in $lastinc secs) - avg speed : $speed rec/min </div>";
 			}
-			foreach($errors as $error)
-			{
-				$out.="<div class='log_error'>$error</div>";
-			}
-			foreach($warnings as $warning)
-			{
-				$out.="<div class='log_warning'>$warning</div>";
-			}
-			foreach($indexes as $k=>$v)
-			{
-				$out.="<div class='log_indexing'>$k....$v</div>";
-			}
-			if($skipped>0)
-			{
-				$out.="<div class='log_info'>Skipped $skipped records</div>";
-			}
-			if($ended)
-			{
-				$out.="<script type=\"text/javascript\">setProgress(100);</script>";
-				$out.="<div class='log_end".(count($errors)>0?" log_error":"")."'>";
-				$out.="<span><a href='magmi.php'>Back to Configuration Page</a></span>";
-				$out.="<span><a href='magmi.php?run=1'>Back to Import</a></span>";
-				$out.="</div>";
-				$out.="<script type=\"text/javascript\">endImport();</script>";
-			}
 		}
 	}
-	echo $out;
+	foreach($errors as $error)
+	{
+		$out.="<div class='log_error'>$error</div>";
+	}
+	foreach($warnings as $warning)
+	{
+		$out.="<div class='log_warning'>$warning</div>";
+	}
+	if($skipped>0)
+	{
+		$out.="<div class='log_info'>Skipped $skipped records</div>";
+	}
+	if($ended)
+	{
+		$out.="<script type=\"text/javascript\">setProgress(100);</script>";
+		$out.="<div class='log_end".(count($errors)>0?" log_error":"")."'>";
+		$out.="<span><a href='magmi.php'>Back to Configuration Page</a></span>";
+		$out.="<span><a href='magmi.php?run=1'>Back to Import</a></span>";
+		$out.="</div>";
+		$out.="<script type=\"text/javascript\">endImport();</script>";
+	}
+
+	echo $out.$sout;
 	flush();
-	
+
 }
 catch(Exception $e)
 {
