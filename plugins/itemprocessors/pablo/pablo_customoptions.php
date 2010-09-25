16d076de5d5d0a9a9a2475c8e4056db334fc3076
Sebastien Bracquemont <dweeves@gmail.com>
2010-09-25 00:10:17 +0000

    0.6.5 commit
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk/magmi@71 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- plugins/itemprocessors/pablo/pablo_customoptions.php
+++ plugins/itemprocessors/pablo/pablo_customoptions.php
@@ -4,18 +4,19 @@
  * @author dweeves
  *
  * This class is a sample for item processing
-*/
+ */
 class CustomOptionsItemProcessor extends Magmi_ItemProcessor
 {
-
-    public function getPluginInfo()
-    {
-        return array(
+	private $_containerMap=array("Product Info Column"=>"container1",
+								 "Block after Info Column"=>"container2");
+	public function getPluginInfo()
+	{
+		return array(
             "name" => "Custom Options",
-            "author" => "Pablo",
-            "version" => "0.0.1"
-        );
-    }
+            "author" => "Pablo & Dweeves",
+            "version" => "0.0.2"
+            );
+	}
 
 	public function processItemBeforeId(&$item,$params=null)
 	{
@@ -24,33 +25,30 @@ class CustomOptionsItemProcessor extends Magmi_ItemProcessor
 
 	public function processItemAfterId(&$item,$params=null)
 	{
-        $hasOptions = 0;
-        $requiredOptions = 0;
-        $custom_options = array();
-        $itemCopy = $item;
-        foreach($itemCopy as $field=>$value) {
-            $fieldParts = explode(':', $field);
-            if(count($fieldParts)>2 && $value) {
-                unset($item[$field]);
-                $hasOptions = 1;
-                if(isset($fieldParts[0])) {
-                    $title = $fieldParts[0];
-                }
-                if(isset($fieldParts[1])) {
-                    $type = $fieldParts[1];
-                }
-                if(isset($fieldParts[2])) {
-                    $is_required = $fieldParts[2];
-                    if($is_required) {
-                        $requiredOptions = 1;
-                    }
-                }
-                if(isset($fieldParts[3])) {
-                    $sort_order = $fieldParts[3];
-                }
-                //@list($title,$type,$is_required,$sort_order) = $fieldParts;
-                $title = ucfirst(str_replace('_',' ',$title));
-                $custom_options[] = array(
+		$hasOptions = 0;
+		$requiredOptions = 0;
+		$custom_options = array();
+		$itemCopy = $item;
+
+		foreach($itemCopy as $field=>$value) {
+			$fieldParts = explode(':', $field);
+			if(count($fieldParts)>2 && $value) {
+				$sort_order=0;
+					
+				unset($item[$field]);
+				$hasOptions = 1;
+				$title = $fieldParts[0];
+				$type = $fieldParts[1];
+				$is_required = $fieldParts[2];
+				if($is_required) {
+					$requiredOptions = 1;
+				}
+				if(isset($fieldParts[3])) {
+					$sort_order = $fieldParts[3];
+				}
+				//@list($title,$type,$is_required,$sort_order) = $fieldParts;
+				$title = ucfirst(str_replace('_',' ',$title));
+				$custom_options[] = array(
                     'is_delete'=>0,
                     'title'=>$title,
                     'previous_group'=>'',
@@ -59,55 +57,55 @@ class CustomOptionsItemProcessor extends Magmi_ItemProcessor
                     'is_require'=>$is_required,
                     'sort_order'=>$sort_order,
                     'values'=>array()
-                );
-                $values = explode('|',$value);
-                foreach($values as $v) {
-                    $parts = explode(':',$v);
-                    $title = $parts[0];
-                    if(count($parts)>1) {
-                        $price_type = $parts[1];
-                    } else {
-                        $price_type = 'fixed';
-                    }
-                    if(count($parts)>2) {
-                        $price = $parts[2];
-                    } else {
-                        $price =0;
-                    }
-                    if(count($parts)>3) {
-                        $sku = $parts[3];
-                    } else {
-                        $sku='';
-                    }
-                    if(count($parts)>4) {
-                        $sort_order = $parts[4];
-                    } else {
-                        $sort_order = 0;
-                    }
-                    switch($type) {
-                        case 'file':
-                           /* TODO */
-                           break;
+				);
+				$values = explode('|',$value);
+				foreach($values as $v) {
+					$parts = explode(':',$v);
+					$title = $parts[0];
+					if(count($parts)>1) {
+						$price_type = $parts[1];
+					} else {
+						$price_type = 'fixed';
+					}
+					if(count($parts)>2) {
+						$price = $parts[2];
+					} else {
+						$price =0;
+					}
+					if(count($parts)>3) {
+						$sku = $parts[3];
+					} else {
+						$sku='';
+					}
+					if(count($parts)>4) {
+						$sort_order = $parts[4];
+					} else {
+						$sort_order = 0;
+					}
+					switch($type) {
+						case 'file':
+							/* TODO */
+							break;
 
-                        case 'field':
-                        case 'area':
-                           $custom_options[count($custom_options) - 1]['max_characters'] = $sort_order;
-                           /* NO BREAK */
+						case 'field':
+						case 'area':
+							$custom_options[count($custom_options) - 1]['max_characters'] = $sort_order;
+							/* NO BREAK */
 
-                        case 'date':
-                        case 'date_time':
-                        case 'time':
-                           $custom_options[count($custom_options) - 1]['price_type'] = $price_type;
-                           $custom_options[count($custom_options) - 1]['price'] = $price;
-                           $custom_options[count($custom_options) - 1]['sku'] = $sku;
-                           break;
+						case 'date':
+						case 'date_time':
+						case 'time':
+							$custom_options[count($custom_options) - 1]['price_type'] = $price_type;
+							$custom_options[count($custom_options) - 1]['price'] = $price;
+							$custom_options[count($custom_options) - 1]['sku'] = $sku;
+							break;
 
-                        case 'drop_down':
-                        case 'radio':
-                        case 'checkbox':
-                        case 'multiple':
-                        default:
-                            $custom_options[count($custom_options) - 1]['values'][]=array(
+						case 'drop_down':
+						case 'radio':
+						case 'checkbox':
+						case 'multiple':
+						default:
+							$custom_options[count($custom_options) - 1]['values'][]=array(
                                 'is_delete'=>0,
                                 'title'=>$title,
                                 'option_type_id'=>-1,
@@ -115,95 +113,96 @@ class CustomOptionsItemProcessor extends Magmi_ItemProcessor
                                 'price'=>$price,
                                 'sku'=>$sku,
                                 'sort_order'=>$sort_order,
-                            );
-                            break;
-                    }
-                }
-            }
-        }
+							);
+							break;
+					}
+				}
+			}
+		}
 
-        $pid = $params['product_id'];
+		$pid = $params['product_id'];
 
-        $tname=$this->tablename('catalog_product_entity');
-        $data = array($hasOptions, $requiredOptions, $pid);
-        $sql="UPDATE `$tname` SET has_options=?,required_options=? WHERE entity_id=?";
-        $this->update($sql,$data);
+		$tname=$this->tablename('catalog_product_entity');
+		$data = array($hasOptions, $requiredOptions, $pid);
+		$sql="UPDATE `$tname` SET has_options=?,required_options=? WHERE entity_id=?";
+		$this->update($sql,$data);
 
-        $t1 = $this->tablename('catalog_product_option');
-        $t2 = $this->tablename('catalog_product_option_title');
-        $t3 = $this->tablename('catalog_product_option_price');
-        $t4 = $this->tablename('catalog_product_option_type_value');
-        $t5 = $this->tablename('catalog_product_option_type_title');
-        $t6 = $this->tablename('catalog_product_option_type_price');
+		$t1 = $this->tablename('catalog_product_option');
+		$t2 = $this->tablename('catalog_product_option_title');
+		$t3 = $this->tablename('catalog_product_option_price');
+		$t4 = $this->tablename('catalog_product_option_type_value');
+		$t5 = $this->tablename('catalog_product_option_type_title');
+		$t6 = $this->tablename('catalog_product_option_type_price');
 
-        // delete old custom options
-        if(!$params['new_product']) {
-            $sql = "DELETE $t1 FROM $t1 WHERE $t1.product_id=$pid";
-            $this->delete($sql);
-        }
+		// delete old custom options
+		if(!$params['new']) {
+			$sql = "DELETE $t1 FROM $t1 WHERE $t1.product_id=$pid";
+			$this->delete($sql);
+		}
 
-        // create new custom options
-        if(count($custom_options)>0) {
-            $item['options_container'] = 'container2';
+		// create new custom options
+		if(count($custom_options)>0) {
 
-            $optionTitleSql = "INSERT INTO $t2 (option_id, store_id, title) VALUES ";
-            $optionTitleValues = array();
+			$item['options_container'] = $this->_containerMap[$item['options_container']];
 
-            $optionTypeTitleSql = "INSERT INTO $t5 (option_type_id, store_id, title) VALUES ";
-            $optionTypeTitleValues = array();
+			$optionTitleSql = "INSERT INTO $t2 (option_id, store_id, title) VALUES ";
+			$optionTitleValues = array();
 
-            $optionTypePriceSql = "INSERT INTO $t6 (option_type_id, store_id, price, price_type) VALUES ";
-            $optionTypePriceValues = array();
+			$optionTypeTitleSql = "INSERT INTO $t5 (option_type_id, store_id, title) VALUES ";
+			$optionTypeTitleValues = array();
 
-            foreach($custom_options as $option) {
-                $values = array($pid, $option['type'], $option['is_require']);
-                $sql = "INSERT INTO $t1
+			$optionTypePriceSql = "INSERT INTO $t6 (option_type_id, store_id, price, price_type) VALUES ";
+			$optionTypePriceValues = array();
+
+			foreach($custom_options as $option) {
+				$values = array($pid, $option['type'], $option['is_require']);
+				$sql = "INSERT INTO $t1
                         (product_id, type, is_require)
                         VALUES (?, ?, ?)";
-                $optionId = $this->insert($sql, $values);
+				$optionId = $this->insert($sql, $values);
 
-                $optionTitleSql = $optionTitleSql."(?, ?, ?),";
-                $optionTitleValues[] = $optionId;
-                $optionTitleValues[] = 0;
-                $optionTitleValues[] = $option['title'];
+				$optionTitleSql = $optionTitleSql."(?, ?, ?),";
+				$optionTitleValues[] = $optionId;
+				$optionTitleValues[] = 0;
+				$optionTitleValues[] = $option['title'];
 
-                foreach($option['values'] as $val) {
-                    $values = array($optionId, $val['sku'], $val['sort_order']);
-                    $sql = "INSERT INTO $t4
+				foreach($option['values'] as $val) {
+					$values = array($optionId, $val['sku'], $val['sort_order']);
+					$sql = "INSERT INTO $t4
                             (option_id, sku, sort_order)
                             VALUES (?, ?, ?)";
-                    $optionTypeId = $this->insert($sql, $values);
+					$optionTypeId = $this->insert($sql, $values);
 
-                    $optionTypeTitleSql = $optionTypeTitleSql."(?, ?, ?),";
-                    $optionTypeTitleValues[] = $optionTypeId;
-                    $optionTypeTitleValues[] = 0;
-                    $optionTypeTitleValues[] = $val['title'];
+					$optionTypeTitleSql = $optionTypeTitleSql."(?, ?, ?),";
+					$optionTypeTitleValues[] = $optionTypeId;
+					$optionTypeTitleValues[] = 0;
+					$optionTypeTitleValues[] = $val['title'];
 
-                    $optionTypePriceSql = $optionTypePriceSql."(?, ?, ?, ?),";
-                    $optionTypePriceValues[] = $optionTypeId;
-                    $optionTypePriceValues[] = 0;
-                    $optionTypePriceValues[] = $val['price'];
-                    $optionTypePriceValues[] = $val['price_type'];
-                }
-            }
+					$optionTypePriceSql = $optionTypePriceSql."(?, ?, ?, ?),";
+					$optionTypePriceValues[] = $optionTypeId;
+					$optionTypePriceValues[] = 0;
+					$optionTypePriceValues[] = $val['price'];
+					$optionTypePriceValues[] = $val['price_type'];
+				}
+			}
 
-            $optionTitleSql = rtrim($optionTitleSql, ',');
-            $this->insert($optionTitleSql, $optionTitleValues);
+			$optionTitleSql = rtrim($optionTitleSql, ',');
+			$this->insert($optionTitleSql, $optionTitleValues);
 
-            $optionTypeTitleSql = rtrim($optionTypeTitleSql, ',');
-            $this->insert($optionTypeTitleSql, $optionTypeTitleValues);
+			$optionTypeTitleSql = rtrim($optionTypeTitleSql, ',');
+			$this->insert($optionTypeTitleSql, $optionTypeTitleValues);
 
-            $optionTypePriceSql = rtrim($optionTypePriceSql, ',');
-            $this->insert($optionTypePriceSql, $optionTypePriceValues);
-        }
+			$optionTypePriceSql = rtrim($optionTypePriceSql, ',');
+			$this->insert($optionTypePriceSql, $optionTypePriceValues);
+		}
 		return true;
 	}
 
 	/*
-	public function processItemException(&$item,$params=null)
-	{
+	 public function processItemException(&$item,$params=null)
+	 {
 
-	}*/
+	 }*/
 
 	public function initialize($params)
 	{
@@ -212,9 +211,9 @@ class CustomOptionsItemProcessor extends Magmi_ItemProcessor
 
 	public function processColumnList(&$cols,$params=null)
 	{
-        if(!in_array('options_container', $cols)) {
-            $cols[] = 'options_container';
-        }
-        return true;
+		if(!in_array('options_container', $cols)) {
+			$cols[] = 'options_container';
+		}
+		return true;
 	}
 }
