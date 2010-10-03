05b7352a36c619fec0798ee7fef3bc9624a77380
dweeves <dweeves@gmail.com>
2010-10-03 10:42:32 +0000

    changed version to 0.6.7
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@78 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/inc/magmi_importer.php
+++ magmi/inc/magmi_importer.php
@@ -227,7 +227,7 @@ class MagentoMassImporter extends DBHelper
 	public $mode="update";
 	public static $state=null;
 	protected static $_statefile=null;
-	public static $version="0.6.6";
+	public static $version="0.6.7";
 	public $customip=null;
 	public  static $_script=__FILE__;
 	private $_pluginclasses=array();
