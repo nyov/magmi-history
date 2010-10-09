ebc7e903c6249a1aa1d24a0dbca4224434c21090
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2010-10-09 14:23:03 +0000

    autosave of plugins config on import each plugin can declare its parameters, only declared params will be persisted updated existing plugin to take into account saved parameters
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk/magmi@87 69904d63-209b-40b2-b39f-34af9802e1b1

--- plugins/datasources/csv/options_panel.php
+++ plugins/datasources/csv/options_panel.php
@@ -5,12 +5,12 @@ File to import:
 	$magdir=$conf->get("MAGENTO","basedir");
 	$files=glob($magdir."/var/import/*.csv");
 ?>
-<select name="filename">
+<select name="CSV:filename">
 	<?php foreach($files as $fname){ ?>	
-		<option ><?php echo $fname?></option>
+		<option <?php if($fname==$this->getParam("CSV:filename")){?>selected=selected<?php }?>><?php echo $fname?></option>
 	<?php }?>
 </select>
 <div>
-<span class="">CSV separator:</span><input type="text" maxlength="1" size="1" name="csv_separator" value=","></input>
-<span class="">CSV Enclosure:</span><input type="text" maxlength="1" size="1" name="csv_enclosure" value='"'></input>
+<span class="">CSV separator:</span><input type="text" maxlength="1" size="1" name="CSV:separator" value="<?php echo $this->getParam("CSV:separator")?>"></input>
+<span class="">CSV Enclosure:</span><input type="text" maxlength="1" size="1" name="CSV:enclosure" value='<?php echo $this->getParam("CSV:enclosure")?>'></input>
 </div>
