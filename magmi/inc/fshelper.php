ad89efbcccb53ed293659397da0fbdab62e2bb1f
dweeves <dweeves@gmail.com>
2010-11-21 21:32:16 +0000

    fix for permissions lookup, better error display
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@157 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/inc/fshelper.php
+++ magmi/inc/fshelper.php
@@ -7,13 +7,22 @@ class FSHelper
 		$info=stat($dir);
 		$uid=$info["uid"];
 		$gid=$info["gid"];
-		$puid=getmyuid();
-		$pgid=getmygid();
+		if(function_exists("posix_getuid"))
+		{
+			$puid=posix_getuid();
+			$pgid=posix_getgid();
+			
+		}
+		else
+		{
+			$puid=getmyuid();
+			$pgid=getmygid();
+		}
 		list($owner,$grp,$other)=substr(decoct($info["mode"]),2);
 		$v1=($uid==$puid && (intval($owner) & 2)==2);
 		$v2=($gid==$pgid && (intval($grp) & 2)==2);
 		$v3=(intval($other) & 2)==2;
-	 	$ok=( $v1||$v2 || $v3);
+	 	$ok=( $v1|| $v2 || $v3);
 	 	return $ok;
 	 }
 	 
