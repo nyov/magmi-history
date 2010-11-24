16db2e4b7c57554090542fdce07af5b0401581c1
dweeves <dweeves@gmail.com>
2010-11-24 21:14:32 +0000

    added options panel settings
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@175 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/plugins/base/general/importurl/options_panel.php
+++ magmi/plugins/base/general/importurl/options_panel.php
@@ -1,17 +1,39 @@
 <div class="plugin_description">
-This plugin provides a textarea that gives the url mapping of the current settings that can be used by wget or curl
-this area is refreshed each time you leave any field of this import config page.
+This plugin provides a textarea that gives the wget or curl command line (or just url if you want) that will run magmi with
+current settings.
+<p>this area is refreshed each time you leave any field of this import config page.</p>
 </div>
+<select id="GETURL:mode">
+	<option value="wget">wget</option>
+	<option value="curl">curl</option>
+	<option value="rawurl">just url</option>
+</select>
 <div id="GETURL:urlcontainer">
 	<textarea id="GETURL:url" cols="132" rows="5"></textarea>
 </div>
 <script type="text/javascript">
 	magmi_getimporturl=function()
 	{
+		var mode=$F('GETURL:mode');
 		old_action=$('import_form').action;
 		$('import_form').action="./magmi_run.php";
 		var url=$('import_form').action+'?'+$('import_form').serialize();
-		$('GETURL:url').update(url);
+		var content="";
+		switch(mode)
+		{
+			case "wget":
+				content='wget "'+url+'" -O /dev/null';
+				break;
+			case "curl":
+				content='curl -o /dev/null "'+url+'"';
+				break;
+			case "rawurl":
+				content=url;
+				break;
+			default:
+				content=url;
+		}
+		$('GETURL:url').update(content);
 		$('import_form').action=old_action;
 	}
 	Event.observe(window, 'load', function() {
@@ -21,7 +43,9 @@ this area is refreshed each time you leave any field of this import config page.
 				{
 					it.observe('blur',magmi_getimporturl);
 				}});
+		$('GETURL:mode').observe('change',magmi_getimporturl);
 		magmi_getimporturl();
+		
 	});
 	
 </script>
