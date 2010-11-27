bc8b140197058f13e293a5c053cf791e5d48aaf4
dweeves <dweeves@gmail.com>
2010-11-27 15:02:26 +0000

    set import limiter to version 0.0.2
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@191 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/plugins/base/itemprocessors/importlimiter/01_importlimiter.php
+++ magmi/plugins/base/itemprocessors/importlimiter/01_importlimiter.php
@@ -9,7 +9,7 @@ class ImportLimiter extends Magmi_ItemProcessor
 	{
 		return array("name"=>"Magmi Import Limiter",
 					 "author"=>"Dweeves",
-					 "version"=>"0.0.1");
+					 "version"=>"0.0.2");
 	}
 	
 	
