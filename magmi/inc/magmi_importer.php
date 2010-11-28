b0f8ad64b9b2be432a1a645c535342bcfa096805
Sebastien Bracquemont <dweeves@gmail.com>
2010-11-28 14:42:47 +0000

    set version tag as alpha2
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@195 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/inc/magmi_importer.php
+++ magmi/inc/magmi_importer.php
@@ -53,7 +53,7 @@ class MagentoMassImporter extends DBHelper
 	public $mode="update";
 	public static $state=null;
 	protected static $_statefile=null;
-	public static $version="0.6.16-alpha";
+	public static $version="0.6.16-alpha2";
 	public $customip=null;
 	public  static $_script=__FILE__;
 	private $_pluginclasses=array();
