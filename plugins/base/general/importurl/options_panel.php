e82e47496d676be21a58ea3ddc425f124b341b05
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-02-15 22:16:58 +0000

    fixed importurlui plugin
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk/magmi@276 69904d63-209b-40b2-b39f-34af9802e1b1

--- plugins/base/general/importurl/options_panel.php
+++ plugins/base/general/importurl/options_panel.php
@@ -12,13 +12,14 @@ current settings.
 	<textarea id="GETURL:url" cols="132" rows="5"></textarea>
 </div>
 <script type="text/javascript">
-/*
+
 magmi_getimporturl=function()
 	{
-		var mode=$F('GETURL:mode');
-		old_action=$('import_form').action;
-		$('import_form').action="./magmi_run.php";
-		var url=$('import_form').action+'?'+$('import_form').serialize();
+		var mode=$('GETURL:mode').value;
+		var old_action=$('runmagmi').action;
+		$('runmagmi').action="./magmi_run.php";
+		var url=$('runmagmi').action+'?'+Form.serializeElements([$('mode'),$('runprofile')]);
+		$('runmagmi').action=old_action;
 		var content="";
 		switch(mode)
 		{
@@ -35,18 +36,15 @@ magmi_getimporturl=function()
 				content=url;
 		}
 		$('GETURL:url').update(content);
-		$('import_form').action=old_action;
 	}
-	Event.observe(window, 'load', function() {
+	
 		
-		$('import_form').getElements().each(function(it){
-				if(it.id!='GETURL:urlcontainer')
-				{
-					it.observe('blur',magmi_getimporturl);
-				}});
-		$('GETURL:mode').observe('change',magmi_getimporturl);
-		magmi_getimporturl();
+	$('runmagmi').getElements().each(function(it){
+				it.observe('blur',magmi_getimporturl);
+		});
+	$('GETURL:mode').observe('change',magmi_getimporturl);
+	magmi_getimporturl();
 		
-	});
-	*/
+	
+	
 </script>
