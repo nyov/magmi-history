15f2201680ecccca9a2d26320ca630c27bf05b5d
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-02-24 01:59:14 +0000

    added magmi optimizer plugin
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk/magmi@311 69904d63-209b-40b2-b39f-34af9802e1b1

---
+++ plugins/base/general/optimizer/magmi_optimizer_plugin.php
@@ -0,0 +1,25 @@
+<?php
+class Magmi_OptimizerPlugin extends Magmi_GeneralImportPlugin
+{
+	public function getPluginInfo()
+	{
+		return array("name"=>"Magmi Optimizer",
+					 "author"=>"Dweeves",
+					 "version"=>"1.0.0");
+	}
+	
+	public function beforeImport()
+	{
+		$this->log("Optimizing magmi","info");
+		$eaov=$this->tablename("eav_attribute_option_value");
+		$sql="ALTER IGNORE TABLE $eaov ADD INDEX MAGMI_EAOV_OPTIMIZATION_IDX (`value`)";
+		$this->exec_stmt($sql);
+		return true;
+	}
+	
+		
+	public function initialize($params)
+	{
+		
+	}
+}
