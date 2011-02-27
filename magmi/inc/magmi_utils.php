8c8763f1b5b8d3a66aac4a58effe397ddff6c50b
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-02-27 13:54:55 +0000

    some cleanup in classes
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@318 69904d63-209b-40b2-b39f-34af9802e1b1

---
+++ magmi/inc/magmi_utils.php
@@ -0,0 +1,23 @@
+<?php
+//utilities function
+// return null for empty string
+function nullifempty($val)
+{
+	return (isset($val)?(trim($val)==""?null:$val):null);
+}
+// return false for empty string
+function falseifempty($val)
+{
+	return (isset($val)?(strlen($val)==0?false:$val):false);
+}
+//test for empty string
+function testempty($arr,$val)
+{
+	
+	return !isset($arr[$val]) || strlen(trim($arr[$val]))==0;
+}
+
+function deleteifempty($val)
+{
+	return (isset($val)?(trim($val)==""?"__MAGMI_DELETE__":$val):"__MAGMI_DELETE__");
+}
