66d4d696e9ba62151363df6623d34b2cf315c862
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-03-18 13:51:15 +0000

    New "value replacer" plugin, allows to create / modify values from the original read ones can accept both standard strings with {item.xxx} values AND php expressions in {{...}}
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@412 69904d63-209b-40b2-b39f-34af9802e1b1

---
+++ magmi/plugins/extra/itemprocessors/valuereplacer/valuereplacer.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Class SampleItemProcessor
+ * @author dweeves
+ *
+ * This class is a sample for item processing   
+*/ 
+class ValueReplacerItemProcessor extends Magmi_ItemProcessor
+{
+
+	protected $_rvals=array();
+	
+    public function getPluginInfo()
+    {
+        return array(
+            "name" => "Value Replacer",
+            "author" => "Dweeves",
+            "version" => "0.0.1"
+        );
+    }
+	
+	
+    public function parseval($pvalue,$item)
+	{
+		$matches=array();
+		//replacing item values
+		while(preg_match("|\{item\.(.*?)\}|",$pvalue,$matches))
+		{
+			foreach($matches as $match)
+			{
+				if($match!=$matches[0])
+				{
+					if(isset($item[$match]))
+					{
+						$rep=$item[$match];
+					}
+					else
+					{
+						$rep="";
+					}
+					$pvalue=str_replace($matches[0],$rep,$pvalue);
+				}
+			}
+		}
+		unset($matches);
+		//replacing expr values
+		while(preg_match("|\{\{(.*?)\}\}|",$pvalue,$matches))
+		{
+			foreach($matches as $match)
+			{
+				if($match!=$matches[0])
+				{
+					$code=$match;
+					$rep=eval("return ($code);");
+					$pvalue=str_replace($matches[0],$rep,$pvalue);							
+				}				
+			}
+		}
+		
+		unset($matches);
+		return $pvalue;
+	}
+	
+	public function processItemBeforeId(&$item,$params)
+	{
+		foreach($this->_rvals as $attname=>$pvalue)
+		{
+			if(isset($item[$attname]))
+			{
+				$item[$attname]=$this->parseval($pvalue,$item);
+			}
+		}
+		return true;
+	}
+	
+	public function initialize($params)
+	{
+		foreach($params as $k=>$v)
+		{
+			if(preg_match_all("/^VREP:(.*)$/",$k,$m) && $k!="VREP:columnlist")
+			{
+				$colname=rawurldecode($m[1][0]);
+				$this->_rvals[$colname]=$params[$k];
+			}
+		}
+	}
+	
+	//auto add columns if not set 
+	public function processColumnList(&$cols)
+	{
+		$cols=array_unique(array_merge($cols,explode(",",$this->getParam("VREP:columnlist"))));
+	}
+	
+	public function getPluginParams($params)
+	{
+		$pp=array();
+		foreach($params as $k=>$v)
+		{
+			if(preg_match("/^VREP:.*$/",$k))
+			{
+				$pp[$k]=$v;
+			}
+		}	
+		return $pp;
+	}	
+}
