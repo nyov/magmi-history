0f6c382dd0e1f4bd766ce1b73f52658a5c55c0dd
dweeves <dweeves@gmail.com>
2011-04-29 22:03:13 +0000

    added index on eav_attribute_name
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@524 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/plugins/base/general/optimizer/magmi_optimizer_plugin.php
+++ magmi/plugins/base/general/optimizer/magmi_optimizer_plugin.php
@@ -5,22 +5,23 @@ class Magmi_OptimizerPlugin extends Magmi_GeneralImportPlugin
 	{
 		return array("name"=>"Magmi Optimizer",
 					 "author"=>"Dweeves",
-					 "version"=>"1.0.4");
+					 "version"=>"1.0.5");
 	}
 	
 	public function beforeImport()
 	{
-		$tbls=array("eav_attribute_option_value"=>"MAGMI_EAOV_OPTIMIZATION_IDX",
-					"catalog_product_entity_media_gallery"=>"MAGMI_CPEM_OPTIMIZATION_IDX",
-					"catalog_category_entity_varchar"=>"MAGMI_CCEV_OPTIMIZATION_IDX");
+		$tbls=array("eav_attribute_option_value"=>array("value","MAGMI_EAOV_OPTIMIZATION_IDX"),
+					"catalog_product_entity_media_gallery"=>array("value","MAGMI_CPEM_OPTIMIZATION_IDX"),
+					"catalog_category_entity_varchar"=>array("value","MAGMI_CCEV_OPTIMIZATION_IDX"),
+					"eav_attribute"=>array("attribute_code","MAGMI_EA_CODE_OPTIMIZATION_IDX"));
 		$this->log("Optimizing magmi","info");
-		foreach($tbls as $tblname=>$idxname)
+		foreach($tbls as $tblname=>$idxinfo)
 		{
 			try
 			{
 				$t=$this->tablename($tblname);
-				$this->log("Adding index $idxname on $t","info");
-				$sql="ALTER  TABLE $t ADD INDEX $idxname (`value`)";
+				$this->log("Adding index {$idxinfo[1]} on $t","info");
+				$sql="ALTER  TABLE $t ADD INDEX {$idxinfo[1]} (`{$idxinfo[0]}`)";
 				$this->exec_stmt($sql);
 			}
 			catch(Exception $e)
