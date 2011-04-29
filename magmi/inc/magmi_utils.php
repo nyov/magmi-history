549e31433644aca3e275504e9f75760f2cb8c8f1
dweeves <dweeves@gmail.com>
2011-04-29 22:01:59 +0000

    added Slugger utility class
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@523 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/inc/magmi_utils.php
+++ magmi/inc/magmi_utils.php
@@ -31,3 +31,31 @@ function csl2arr($cslarr,$sep=",")
 	}
 	return $arr;
 }
+
+class Slugger
+{
+	static protected $_translit=array(
+    'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+    'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+    'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+    'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+    'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+    'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+    'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f'
+		);
+	
+	public static function stripAccents($text){
+		
+		return strtr($text,self::$_translit);
+	}
+
+	public static function slug($str,$allowslash=false)
+	{
+      $str = strtolower(self::stripAccents(trim($str)));
+      $rerep=$allowslash?'[^a-z0-9-/]':'[^a-z0-9-]';
+      $str = preg_replace("|$rerep|", '-', $str);
+      $str = preg_replace('|-+|', "-", $str);
+      $str = preg_replace('|-$|', "", $str);
+      return $str;
+	}
+}
