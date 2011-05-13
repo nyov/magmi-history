b0e739638578001236f191cbddbc6887ee3e4e7f
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-05-13 21:26:35 +0000

    added join for sku
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk/magmi@565 69904d63-209b-40b2-b39f-34af9802e1b1

--- plugins/utilities/customsql/prequests/resetstock.sql
+++ plugins/utilities/customsql/prequests/resetstock.sql
@@ -1,2 +1,4 @@
--- test multi request;
-UPDATE [[tn:cataloginventory_stock_item]] SET qty=[[qty/quantity]] WHERE product_id REGEXP [[sku_regexp/target sku regexp]];
+-- reset stock for a given sku regex;
+UPDATE [[tn:cataloginventory_stock_item]] as csi 
+JOIN [[tn:catalog_product_entity]] as cpe ON csi.product_id=cpe.entity_id AND cpe.sku REGEXP [[sku_regexp/target sku regexp/.*]]
+SET csi.qty=[[qty/quantity/0]];
