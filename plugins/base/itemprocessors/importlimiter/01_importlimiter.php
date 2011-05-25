455434df0dc725d42ea5bdad039c406745f7d095
Sebastien Bracquemont <dweeves@gmail.com>
2011-05-25 22:21:15 +0000

    update version
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk/magmi@586 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- plugins/base/itemprocessors/importlimiter/01_importlimiter.php
+++ plugins/base/itemprocessors/importlimiter/01_importlimiter.php
@@ -9,7 +9,7 @@ class ImportLimiter extends Magmi_ItemProcessor
 	{
 		return array("name"=>"Magmi Import Limiter",
 					 "author"=>"Dweeves",
-					 "version"=>"0.0.4");
+					 "version"=>"0.0.4a");
 	}
 	
 	
