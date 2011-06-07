be76bb03eebf796066934ef8a0ba1a8363993d98
dweeves <dweeves@gmail.com>
2011-06-07 22:50:57 +0000

    much nicer display of header & infos added logo by dewi
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@621 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/web/header.php
+++ magmi/web/header.php
@@ -1,12 +1,27 @@
 <?php require_once("head.php")?>
 <div class="container_12">
 	<div class="grid_12 title ">
-		<span>
-  		MAGMI (MAGento Mass Importer) by Dweeves
+		<div class="logo">
+		<div class="logoauth">
+		Logo Design by dewi
+		</div>
+		</div>
+		<div class="info">
+		<h3>Release Information</h3>
+		<div class="version">
+ 			v<?php echo Magmi_Version::$version ?>
+ 		</div>
+		<div class="author">
+  		Provided to the community by <b><i><a href="mailto:dweeves@gmail.com">Dweeves</a></i></b>
+ 		</div>
+ 		<div class="license">
+ 		Released under <a href="javascript:;">MIT OSL License
+ 		<span class="hover">
+ 		<?php echo nl2br(file_get_contents("../inc/license.txt"))?>
  		</span>
- 		<span class="version">
- 		version <?php echo Magmi_Version::$version ?>
- 		</span>
+ 		</a>
+ 		</div>
+ 		</div>
 	</div>
 </div>
 
