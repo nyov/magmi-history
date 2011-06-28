b90d2aa1204fb9343d6b9259a188386341b079b5
dweeves <dweeves@gmail.com>
2011-06-28 00:55:03 +0000

    added config change detection management
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@662 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/web/magmi_import_run.php
+++ magmi/web/magmi_import_run.php
@@ -1,13 +1,16 @@
+	<script type="text/javascript">
+		var imp_params={engine:'magmi_productimportengine:Magmi_ProductImportEngine'};
 	<?php 
 	ini_set('magic_gpc_quotes',0);
-	$logfile=isset($_REQUEST['logfile'])?$_REQUEST['logfile']:Magmi_StateManager::getProgressFile();
-	$profile=isset($_REQUEST['profile'])?$_REQUEST['profile']:'default';
+	$profile=isset($_REQUEST["profile"])?$_REQUEST["profile"]:'default';
 	$_SESSION["last_runned_profile"]=$profile;
-	
-	$mode=isset($_REQUEST['mode'])?$_REQUEST['mode']:null;
 	session_write_close();
-	
+	foreach($_REQUEST as $k=>$v)
+	{
+		echo "imp_params['$k']='$v';\n";	
+	}
 	?>
+	</script>
 	<div class="clear"></div>
 	<div id="import_log" class="container_12">
 		<div class="section_title grid_12">
@@ -49,24 +52,22 @@
 		}
 	}
 
-	startProgress=function()
+	startProgress=function(imp_params)
 	{
 		window.upd=new Ajax.PeriodicalUpdater("runlog","magmi_progress.php",{frequency:1,evalScripts:true,parameters:{
-		logfile:'<?php echo addslashes($logfile) ?>'}});
+		logfile:imp_params['logfile']}});
 	}
 	
-	startImport=function(filename)
+	startImport=function(imp_params)
 	{
+		
 		if(window._sr==null)
 		{
 			updateTime('startimport_div','Import Started');
 			var rq=new Ajax.Request('magmi_run.php',{method:'post',
-								 parameters:{profile:'<?php echo $profile?>',
-						 		 mode:'<?php echo $mode?>',
-						 		 logfile:'<?php echo addslashes($logfile)?>',
-							 	 engine:'magmi_productimportengine:Magmi_ProductImportEngine'},
+								 parameters:imp_params,
 								onCreate:function(r){window._sr=r}});
-			startProgress.delay(0.5);
+			startProgress(imp_params).delay(0.5);
 		}
 	}
 	
@@ -86,10 +87,12 @@
 		}
 	}
 
-	<?php if($mode!==null){?>
-	startImport();
-	<?php }else{
-	?>
-	startProgress();
-	<?php }?>
+	if(imp_params.mode!==null)
+	{
+		startImport(imp_params);
+	}
+	else
+	{
+		startProgress(imp_params);
+	}
 </script>
