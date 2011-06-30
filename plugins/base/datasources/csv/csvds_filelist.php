a116908a62ec7d8ad56946294ba2340fb3d99396
Sebastien Bracquemont <dweeves@gmail.com>
2011-06-30 19:56:26 +0000

    dynamic change csvdl
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk/magmi@670 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- plugins/base/datasources/csv/csvds_filelist.php
+++ plugins/base/datasources/csv/csvds_filelist.php
@@ -1,4 +1,4 @@
-<select name="CSV:filename">
+<select name="CSV:filename" id="csvfile">
 	<?php 
 	$files=$this->getCSVList();
 	?>
@@ -6,4 +6,9 @@
 		<option <?php if($fname==$this->getParam("CSV:filename")){?>selected=selected<?php }?>><?php echo $fname?></option>
 	<?php }?>
 </select>
-<a href="./download_file.php?file=<?php echo $this->getParam("CSV:filename")?>">Download CSV</a>
+<a id='csvdl' href="./download_file.php?file=<?php echo $this->getParam("CSV:filename")?>">Download CSV</a>
+<script type="text/javascript">
+ $('csvfile').observe('change',function(el){
+ 		$('csvdl').href="./download_file.php?file="+$('csvfile').value;}
+	);
+</script>
