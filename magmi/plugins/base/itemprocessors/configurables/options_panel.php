85da22af9201aabb27a62e24fcc3501a89325633
dweeves <dweeves@gmail.com>
2011-06-30 19:40:20 +0000

    added handling for visibility update
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@668 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/plugins/base/itemprocessors/configurables/options_panel.php
+++ magmi/plugins/base/itemprocessors/configurables/options_panel.php
@@ -8,7 +8,17 @@ This plugins handles configurable import</b>
 	<option value="1" <?php if ($this->getParam("CFGR:simplesbeforeconf")==1){?>selected="selected"<?php }?>>Yes</option></select></li>
 </ul>
 <ul  class="formline">
-<li>
-<input type="checkbox" name="CFGR:updsimplevis" <?php if($this->getParam("CFGR:updsimplevis",false)==true){?>checked="checked"<?php }?>>Make associated simples not visible
+<li class="label">
+Force simples visibility
+</li>
+<li class="value">
+<?php $v=$this->getParam("CFGR:updsimplevis",0)?>
+<select name=CFGR:updsimplevis">
+	<option value="0" <?php if($v==0){?>selected="selected"<?php }?>>Keep Untouched</option>
+	<option value="1" <?php if($v==1){?>selected="selected"<?php }?>>Force to "Not Visible Individually"</option>
+	<option value="2" <?php if($v==2){?>selected="selected"<?php }?>>Force to Catalog</option>
+	<option value="3" <?php if($v==3){?>selected="selected"<?php }?>>Force to Search</option>
+	<option value="4" <?php if($v==4){?>selected="selected"<?php }?>>Force to Catalog, Search</option>
+</select>
 </li>
 </ul>
