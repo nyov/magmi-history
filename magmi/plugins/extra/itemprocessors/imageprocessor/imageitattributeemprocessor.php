651073a9aad84d1ac5b023190283bc353d6e7606
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-08-02 11:23:44 +0000

    fix version
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@703 69904d63-209b-40b2-b39f-34af9802e1b1

--- magmi/plugins/extra/itemprocessors/imageprocessor/imageitattributeemprocessor.php
+++ magmi/plugins/extra/itemprocessors/imageprocessor/imageitattributeemprocessor.php
@@ -39,7 +39,7 @@ class ImageAttributeItemProcessor extends Magmi_ItemProcessor
 		return array(
             "name" => "Image attributes processor",
             "author" => "Dweeves",
-            "version" => "1.0.17",
+            "version" => "1.0.17a",
 			"url"=>"http://sourceforge.net/apps/mediawiki/magmi/index.php?title=Image_attributes_processor"
             );
 	}
