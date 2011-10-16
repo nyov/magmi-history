da5c65549ae7c565814f8422cbfda2f635ef511c
dweeves <dweeves@gmail.com>
2011-10-16 14:45:33 +0000

    fixed options panel for mysql options
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@731 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/plugins/base/datasources/genericsql/mysql_options.php
+++ magmi/plugins/base/datasources/genericsql/mysql_options.php
@@ -1,24 +1,24 @@
 <div id="mysql_config">
 <ul class="formline">
 <li class="label">Input DB Host</li>
-<li class="value"><input type="text" name="SQL:dbhost" value="<?echo $this->getParam("SQL:dbhost","localhost")?>"/></li>
+<li class="value"><input type="text" name="SQL:dbhost" value="<?php echo $this->getParam("SQL:dbhost","localhost")?>"/></li>
 </ul>
 <ul class="formline">
 <li class="label">Input DB Name</li>
-<li class="value"><input type="text" name="SQL:dbname" value="<?echo $this->getParam("SQL:dbname","")?>"/></li>
+<li class="value"><input type="text" name="SQL:dbname" value="<?php echo $this->getParam("SQL:dbname","")?>"/></li>
 </ul>
 <ul class="formline">
 <li class="label">Input DB User</li>
-<li class="value"><input type="text" name="SQL:dbuser" value="<?echo $this->getParam("SQL:dbuser","")?>"/></li>
+<li class="value"><input type="text" name="SQL:dbuser" value="<?php echo $this->getParam("SQL:dbuser","")?>"/></li>
 </ul>
 <ul class="formline">
 <li class="label">Input DB Password</li>
-<li class="value"><input type="password" name="SQL:dbpass" value="<?echo $this->getParam("SQL:dbpass","")?>"/></li>
+<li class="value"><input type="password" name="SQL:dbpass" value="<?php echo $this->getParam("SQL:dbpass","")?>"/></li>
 </ul>
 <ul class="formline">
 <li class="label">Input DB Initial Statements (optional)</li>
 <li class="value"><textarea name="SQL:dbextra" cols="80" rows="5">
-<?echo $this->getParam("SQL:dbextra","")?>
+<?php echo $this->getParam("SQL:dbextra","")?>
 </textarea>
 <div class="fieldinfo">
 Put DB requests like SET NAMES if necessary separated by ;
