6048c3d3fe65db0d06d6b03d5e1275eac92db9aa
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-10-16 12:21:22 +0000

    added remove all orphans utility fixed remove orphans
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@725 69904d63-209b-40b2-b39f-34af9802e1b1

--- magmi/plugins/utilities/customsql/prequests/removeallorphans.sql
+++ magmi/plugins/utilities/customsql/prequests/removeallorphans.sql
@@ -4,7 +4,7 @@ FROM [[tn:eav_attribute]] as ea
 JOIN [[tn:eav_attribute_option]] as eao ON eao.attribute_id=ea.attribute_id 
 JOIN [[tn:eav_attribute_option_value]] as eaov ON eaov.option_id=eao.option_id
 LEFT JOIN [[tn:catalog_product_entity_int]] as cpei ON cpei.attribute_id=ea.attribute_id  AND cpei.value=eao.option_id
-WHERE ea.frontend_input='select' AND ea.entity_type_id=4 AND ea.attribute_code=[[attcode/attribute_code]] AND ea.is_user_defined=1
+WHERE ea.frontend_input='select' AND ea.entity_type_id=4  AND ea.is_user_defined=1
 GROUP BY eao.option_id
 HAVING ce=0
 UNION
@@ -14,7 +14,7 @@ JOIN [[tn:eav_attribute_option]] as eao ON eao.attribute_id=ea.attribute_id
 JOIN [[tn:eav_attribute_option_value]] as eaov ON eaov.option_id=eao.option_id
 LEFT JOIN [[tn:catalog_product_entity_varchar]] as cpev 
 ON cpev.attribute_id=ea.attribute_id  AND cpev.value REGEXP CONCAT('.*,?',eaov.value,',?.*')
-WHERE ea.frontend_input='multiselect' AND ea.entity_type_id=4 AND ea.attribute_code=[[attcode/attribute_code]] AND ea.is_user_defined=1
+WHERE ea.frontend_input='multiselect' AND ea.entity_type_id=4 AND  ea.is_user_defined=1
 GROUP BY eao.option_id
 HAVING ce=0
 ) as candidates);
