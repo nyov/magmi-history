f1337893ae6478a373574a87e52d527517f62c27
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-11-22 00:46:02 +0000

    relative to absolute migration
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@800 69904d63-209b-40b2-b39f-34af9802e1b1

--- magmi/plugins/base/datasources/csv/csvds_filelist.php
+++ magmi/plugins/base/datasources/csv/csvds_filelist.php
@@ -3,10 +3,10 @@
 if($files!==false && count($files)>0){?>
 <select name="CSV:filename" id="csvfile">
 	<?php foreach($files as $fname){ ?>	
-		<option <?php if($fname==$this->getParam("CSV:filename")){?>selected=selected<?php }?>><?php echo $fname?></option>
+		<option <?php if($fname==$this->getAbsPath($this->getParam("CSV:filename"))){?>selected=selected<?php }?>><?php echo $fname?></option>
 	<?php }?>
 </select>
-<a id='csvdl' href="./download_file.php?file=<?php echo $this->getParam("CSV:filename")?>">Download CSV</a>
+<a id='csvdl' href="./download_file.php?file=<?php echo $this->getAbsPath($this->getParam("CSV:filename"))?>">Download CSV</a>
 <script type="text/javascript">
  $('csvfile').observe('change',function(el){
  		$('csvdl').href="./download_file.php?file="+$('csvfile').value;}
