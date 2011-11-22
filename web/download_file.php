486af3b0c5f601906397f5fd106f58392a141b96
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-11-22 00:49:49 +0000

    fixed download name
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk/magmi@801 69904d63-209b-40b2-b39f-34af9802e1b1

--- web/download_file.php
+++ web/download_file.php
@@ -17,11 +17,15 @@ $time = time();
  
 // Send file headers
 header("Content-type: $type");
-header("Content-Disposition: attachment;filename=$file");
+header("Content-Disposition: attachment;filename=".basename($file));
 header("Content-Transfer-Encoding: binary");
 header('Pragma: no-cache');
 header('Expires: 0');
 // Send the file contents.
 set_time_limit(0);
 readfile($file);
+}
+else
+{
+	print_r($err);
 }
