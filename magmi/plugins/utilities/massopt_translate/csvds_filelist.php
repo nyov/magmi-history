eb3cdfaa115ccff0fdb1646e0535653ffebdf356
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2011-11-30 01:37:30 +0000

    implemented mass option translater
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@804 69904d63-209b-40b2-b39f-34af9802e1b1

---
+++ magmi/plugins/utilities/massopt_translate/csvds_filelist.php
@@ -0,0 +1,16 @@
+<?php 
+	$files=$this->getCSVList();
+if($files!==false && count($files)>0){?>
+<select name="CSV:filename" id="csvfile">
+	<?php foreach($files as $fname){ ?>	
+		<option <?php if($fname==$this->getAbsPath($this->getParam("CSV:filename"))){?>selected=selected<?php }?>><?php echo $fname?></option>
+	<?php }?>
+</select>
+<a id='csvdl' href="./download_file.php?file=<?php echo $this->getAbsPath($this->getParam("CSV:filename"))?>">Download CSV</a>
+<script type="text/javascript">
+ $('csvfile').observe('change',function(el){
+ 		$('csvdl').href="./download_file.php?file="+$('csvfile').value;}
+	);
+</script><?php } else {?>
+<span> No csv files found in <?php echo $this->getScanDir()?></span>
+	<?php }?>
