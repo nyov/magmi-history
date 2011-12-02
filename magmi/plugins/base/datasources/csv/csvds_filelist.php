889808c9acaf7a7a124837885e21ebf1f54828a3
dweeves <dweeves@gmail.com>
2011-12-02 00:45:46 +0000

    better output & behaviour
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@814 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/plugins/base/datasources/csv/csvds_filelist.php
+++ magmi/plugins/base/datasources/csv/csvds_filelist.php
@@ -3,14 +3,15 @@
 if($files!==false && count($files)>0){?>
 <select name="CSV:filename" id="csvfile">
 	<?php foreach($files as $fname){ ?>	
-		<option <?php if($fname==$this->getAbsPath($this->getParam("CSV:filename"))){?>selected=selected<?php }?>><?php echo $fname?></option>
+		<option <?php if($fname==$this->getAbsPath($this->getParam("CSV:filename"))){?>selected=selected<?php }?> value="<?php echo $fname?>"><?php echo basename($fname)?></option>
 	<?php }?>
 </select>
 <a id='csvdl' href="./download_file.php?file=<?php echo $this->getAbsPath($this->getParam("CSV:filename"))?>">Download CSV</a>
 <script type="text/javascript">
- $('csvfile').observe('change',function(el){
- 		$('csvdl').href="./download_file.php?file="+$('csvfile').value;}
+ $('csvdl').observe('click',function(el){
+	    var fval=$('csvfile').value;
+ 		$('csvdl').href="./download_file.php?file="+fval;}
 	);
 </script><?php } else {?>
-<span> No csv files found in <?php echo $this->getScanDir()?></span>
+<span> No csv files found in <?php echo $this->getScanDir(false)?></span>
 	<?php }?>
