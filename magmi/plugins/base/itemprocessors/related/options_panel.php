642128c1d1003e7ff5ebe649b9c1086ce8f55159
dweeves <dweeves@69904d63-209b-40b2-b39f-34af9802e1b1>
2012-02-20 21:15:21 +0000

    opened GUI option
    git-svn-id: https://magmi.svn.sourceforge.net/svnroot/magmi/trunk@845 69904d63-209b-40b2-b39f-34af9802e1b1

---
+++ magmi/plugins/base/itemprocessors/related/options_panel.php
@@ -0,0 +1,6 @@
+<div class="plugin_description">
+This plugin enable to handle relation &amp; cross relation between products
+</div>
+<div class="">
+<input type="checkbox" name="XREL:full_relate" <?php if($this->getParam("XREL:full_relate",false)==true){?>checked=checked<?php }?>>Regexp relate xre_skus with each other
+</div>
