1fa14a26f9074f7a4bc38480df1bc13f3ae93b11
dweeves <dweeves@gmail.com>
2012-04-24 00:17:48 +0000

    fixed copy profile
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/branches/0.8@882 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/web/magmi_chooseprofile.php
+++ magmi/web/magmi_chooseprofile.php
@@ -13,7 +13,7 @@ if($newprofile!="")
 
 	$bcfg=new EnabledPlugins_Config($ph->getEngine()->getProfilesDir(),$currentprofile);
 	$confdir=Magmi_Config::getInstance()->getConfDir();
-	$npdir=$confdir.DS.$newprofile;
+	$npdir=$confdir.DS.$ph->getEngine()->getProfilesDir().DS.$newprofile;
 	mkdir($npdir,Magmi_Config::getInstance()->getDirMask());
 	$cpdir=$bcfg->getProfileDir();
 	$filelist=scandir($cpdir);
@@ -31,6 +31,6 @@ else
 }
 $_SESSION["engineclass"]=$eng;
 $_SESSION["profile"]=$newprofile;
-session_write_close();
+//session_write_close();
 header("Location:magmi.php");
 
