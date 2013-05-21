b0a0300201bbce7fc639daa1e56fb7147079198a
dweeves <dweeves@gmail.com>
2013-05-21 22:18:56 +0000

    more accurate abspath with no resolving algorithm
    git-svn-id: svn://svn.code.sf.net/p/magmi/code/trunk@1003 8a1fb3a6-a994-4f0a-9076-b49fc636424d

--- magmi/inc/magmi_utils.php
+++ magmi/inc/magmi_utils.php
@@ -76,9 +76,21 @@ function abspath($path,$basepath="",$resolve=true)
 	}
 	else
 	{
-		$abs=preg_replace('|\w+/\.\.\/|', '',$cpath );
-		$abs=preg_replace('|\./|','',$abs);
-	
+		$inparts=explode("/",$cpath);
+		$outparts=array();
+		for($i=0;$i<count($inparts);$i++)
+		{
+			if($inparts[$i]=='..')
+			{
+				array_pop($outparts)	;
+			}
+			else
+			if($inparts[$i]!='.')
+			{
+				$outparts[]=$inparts[$i];
+			}
+		}
+		$abs=implode("/",$outparts);
 	}
 	return $abs;
 }
@@ -147,3 +159,4 @@ class Slugger
       return $str;
 	}
 }
+
